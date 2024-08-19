de7131f45f3ba159586ea2dd7f0ba1aeef2b2dd7
mustafa ferhat <mustafaferhat51800@outlook.com>
2024-08-19 22:44:45 +0300

    first commit

---
+++ File.cs
@@ -0,0 +1,61 @@
+﻿using AYRUZ_YATT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CommonFunctions
+{
+    public class File
+    {
+        public static bool Save(string path, string text)
+        {
+            try
+            {
+                System.IO.File.WriteAllText(path, text);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (ex.HResult == -2147024893)//{"Could not find a part of the path 'C:\\AYRUZ_YATT__\\bin\\Debug\\net8.0-windows\\Folder_Configs\\Tests\\______Steps\\TestInfo.ayruz'."}
+                {
+                    var path_split = path.Split(Path.DirectorySeparatorChar);
+                    string a1 = "";
+                    for (int i = 0; i < path_split.Length - 1; i++)
+                    {
+                        a1 += path_split[i] + "\\";
+                        //for (int j = 0; j < path_split.Length; j++)
+                        //{
+
+                        //}
+                    }
+                    //Directory.CreateDirectory(path);
+                    Directory.CreateDirectory(a1);
+                    Save(path, text);
+                }
+                if (ex.HResult == -2147024891)//{"Access to the path                'C:\\AYRUZ_YATT__\\bin\\Debug\\net8.0-windows\\Folder_Configs\\Tests\\______Steps\\TestInfo.ayruz' is denied."}
+                {
+
+                }
+                MessageBox.Show("Error on Saving.!");
+                Logger.log.Fatal(ex + path + text);
+                //throw;
+            }
+            return false;
+        }
+        public static string Open(string path)
+        {
+            try
+            {
+                return System.IO.File.ReadAllText(path);
+            }
+            catch (Exception ex)
+            {
+                Logger.log.Fatal(ex + path);
+            }
+            return "NULL";
+        }
+    }
+}
---
+++ Logger.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYRUZ_YATT
+{
+    internal class Logger
+    {
+        private readonly log4net.ILog log_ = log4net.LogManager.GetLogger("RollingErrorLogFileAppender");
+        public static log4net.ILog log = Instance.log_;
+
+        private static Logger? Works;
+        private static Logger Instance
+        {
+            get
+            {
+
+                var logRepository = log4net.LogManager.GetRepository(Assembly.GetEntryAssembly());
+                log4net.Config.XmlConfigurator.Configure(logRepository, new FileInfo("Folder_Configs\\log4net.config"));
+                Works ??= new Logger();
+                return Works;
+            }
+        }
+    }
+}
